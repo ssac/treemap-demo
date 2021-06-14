134ea4dc321b2e5db5ffa2f550d85ee1833df30a
Sam Suen <suenkating@gmail.com>
2021-06-14 23:01:59 +0800

    added simple unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, getByTestId } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders data table', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const thElement = screen.getByText(/Name/i);
+  expect(thElement).toBeInTheDocument();
+});
+
+test('renders add button', () => {
+  render(<App />);
+  const btnElement = screen.getByText(/Add/i);
+  expect(btnElement).toBeInTheDocument();
+});
+
+test('renders tree map', () => {
+  render(<App />);
+  expect(getByTestId(document.documentElement, 'tree-map')).toBeInTheDocument();
 });
